97af40a5b315a97fc3b88f1dbe56592d91d4762c
Alvin <b01902088@ntu.edu.tw>
2024-03-07 22:10:15 +0800

    remove unused file

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function (msg) {
-    /* We received a message, let's do as instructed */
-    if (msg.action === 'mySnsAlarm') {
-        alert(msg.content);
-    }
-});

00cc4fc2276ce833a621005b5129749b6b30288f
Alvin <b01902088@ntu.edu.tw>
2024-04-05 14:40:41 +0800

    dev: change update data logic in service worker

--- background.js
+++ background.js
@@ -5,30 +5,23 @@ let targetUrls = [];
 const isInTargetUrls = (url) => url && indexOfUrl(url ?? "") != -1;
 const indexOfUrl = (url) => targetUrls.findIndex(v => url.startsWith(v));
 
-let openUrlTime = -1;
+let prevOpenTime = -1;
 let cacheData = undefined;
-let previousUrl = undefined;
+let tabUrlMap = {};
+let currentTabId = undefined;
 
-function updateAlarm(url)
-{
-    if(isInTargetUrls(url))
-        openUrlTime = Date.now();
-    else
+const currentTabIdInit = 
+    (async () =>
     {
-        openUrlTime = -1;
-    }
-    previousUrl = url;
-}
+        let currentTab = await chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true});
+        if(currentTab !== undefined && currentTab.length != 0)
+            currentTabId = currentTab[0].id;
+    })();
 
+const tabUrlMapInit = 
+    chrome.tabs.query({}, tabs => tabUrlMap = Object.fromEntries(tabs.map(t => [t.id, t.url])));
 
-timeSaver.onLocalStorageUrlListChange.addListener(
-    newUrlList =>
-    {
-        targetUrls = newUrlList;
-        updateAlarm(previousUrl);
-    });
-
-const initialization = 
+const dataInit = 
     (async () =>
     {
         console.debug(`begin data initialization`);
@@ -48,42 +41,60 @@ const initialization =
         console.debug(`data initialization finished`);
     })();
 
-chrome.alarms.onAlarm.addListener(
-    async (alarm) =>
+let init = Promise.all([currentTabIdInit, dataInit, tabUrlMapInit]);
+
+timeSaver.onLocalStorageUrlListChange.addListener(
+    newUrlList =>
     {
-        await initialization;
-        
-        if((!cacheData) || openUrlTime == -1 || currentActivateInfo == undefined)
-            return;
-                
-        var currentTime = Date.now();
-        cacheData.totalUsingTime += (currentTime - openUrlTime) / 60000;
-        openUrlTime = currentTime;
-
-        if(cacheData.totalUsingTime >= cacheData.nextAlarmUsingTime)
-        {
-            console.debug(`on alarm : ${JSON.stringify(alarm)}`);
-        
-            cacheData.nextAlarmUsingTime += 
-                timeSaver.alarmTimeInMinutes *
-                Math.ceil((cacheData.totalUsingTime - cacheData.nextAlarmUsingTime) / timeSaver.alarmTimeInMinutes);
-            
-            showAlarmData(cacheData);
-        }
-
-        await timeSaver.saveAlarmData(cacheData);
+        targetUrls = newUrlList;
+        console.info(`new list :\n${JSON.stringify(newUrlList)}`);
+        updateData();
     });
 
-let currentActivateInfo = undefined;
+async function updateData()
+{
+    await init;
+
+    console.debug(`current tab ${currentTabId}, url : ${tabUrlMap[currentTabId]}`);
+
+    if(!isInTargetUrls(tabUrlMap[currentTabId]))
+    {
+        prevOpenTime = -1;
+        return;
+    }
+    
+    if(prevOpenTime == -1)
+    {
+        prevOpenTime = Date.now();
+        return;
+    }
+
+    var currentTime = Date.now();
+    cacheData.totalUsingTime += (currentTime - prevOpenTime) / 60000;
+    prevOpenTime = currentTime;
+
+    if(cacheData.totalUsingTime >= cacheData.nextAlarmUsingTime)
+    {
+        console.debug(`on alarm : ${JSON.stringify(alarm)}`);
+    
+        cacheData.nextAlarmUsingTime += 
+            timeSaver.alarmTimeInMinutes *
+            Math.ceil((cacheData.totalUsingTime - cacheData.nextAlarmUsingTime) / timeSaver.alarmTimeInMinutes);
+        
+        showAlarmData(cacheData);
+    }
+
+    await timeSaver.saveAlarmData(cacheData);
+}
+
+chrome.alarms.onAlarm.addListener((alarm) => updateData());
 
 chrome.tabs.onActivated.addListener(
     async activateInfo =>
     {
-        currentActivateInfo = activateInfo;
-        console.debug(`activateTab : ${JSON.stringify(currentActivateInfo)}`)
-        let tabInfo = await chrome.tabs.get(activateInfo.tabId);
-        
-        updateAlarm(tabInfo.url);
+        currentTabId = activateInfo.tabId;
+        console.debug(`activateTabId : ${currentTabId}`);
+        updateData();
     });
 
 chrome.tabs.onUpdated.addListener(
@@ -91,10 +102,9 @@ chrome.tabs.onUpdated.addListener(
     {
         if (changeInfo.status != "complete")
             return;
-        console.debug(`update tag, tabId : ${tabId}\ntitle : ${tab.title}\nurl : ${tab.url}`)
-        
-        if(currentActivateInfo !== undefined && tabId == currentActivateInfo.tabId)
-            updateAlarm(tab.url);
+        console.debug(`update tag, tabId : ${tabId}\nurl : ${tab.url}`)
+        tabUrlMap[tabId] = tab.url;
+        updateData();
     });
 
 chrome.tabs.onRemoved.addListener(
@@ -102,10 +112,9 @@ chrome.tabs.onRemoved.addListener(
     {
         console.debug(`closing tab : ${tabId}`);
         
-        if(currentActivateInfo === undefined || currentActivateInfo.tabId != tabId)
-            return;
-
-        updateAlarm(undefined);
+        if(tabUrlMap[tabId] !== undefined)
+            delete tabUrlMap[tabId];
+        updateData();
     });
 
 function showAlarmData(data)

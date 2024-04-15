e9f32e87cc0d800f70d80507de001996716297c7
Alvin <b01902088@ntu.edu.tw>
2024-04-15 20:09:38 +0800

    fix: correct valueHandler's method

--- utility.js
+++ utility.js
@@ -4,48 +4,48 @@ export function AlarmData() {
     this.updateTime = -1;
 };
 
+export function ValueHandler(key, value)
+{
+    this.key = key;
+    this.value = value;
+    this.listeners = [];
+    chrome.storage.local
+        .onChanged
+        .addListener((changes) =>
+            {
+                let targetChange = changes[key];
+                if (targetChange !== undefined)
+                {
+                    this.value = targetChange.newValue;
+                    this.listeners.foreach(callBack => callBack(targetChange.oldValue, targetChange.newValue));
+                }
+            });
+};
+
+ValueHandler.prototype = 
+    {
+        addListener: function(newListener)
+        {
+            if (this.listeners.indexOf(newListener) != -1)
+                return;
+            this.listeners.push(newListener);
+        },
+        removeListener: function(target)
+        {
+            var listenerIndex = this.listeners.indexOf(target);
+            if (listenerIndex == -1)
+                return;
+            this.listeners.slice(listenerIndex, 1);
+        },
+        setValue: function(newValue)
+        {
+            console.debug(`save <${this.key}> : ${JSON.stringify(newValue)}`);
+            return chrome.storage.local.set({[this.key] : newValue});
+        }
+    };
+
 export const timeSaverInitializationPromise = (
     async function () {
-        function ValueHandler(key, value)
-        {
-            this.key = key;
-            this.value = value;
-            this.listeners = [];
-            chrome.storage.local
-                .onChanged
-                .addListener((changes) =>
-                    {
-                        let targetChange = changes[key];
-                        if (targetChange !== undefined)
-                        {
-                            this.value = targetChange.newValue;
-                            this.listeners.foreach(callBack => callBack(targetChange.oldValue, targetChange.newValue));
-                        }
-                    });
-        };
-
-        ValueHandler.prototype = 
-            {
-                addListener: (newListener) =>
-                {
-                    if (this.listeners.indexOf(newListener) != -1)
-                        return;
-                    this.listeners.push(newListener);
-                },
-                removeListener: (target) =>
-                {
-                    var listenerIndex = this.listeners.indexOf(target);
-                    if (listenerIndex == -1)
-                        return;
-                    this.listeners.slice(listenerIndex, 1);
-                },
-                setValue: (newValue) =>
-                {
-                    console.debug(`save <${this.key}> : ${JSON.stringify(newValue)}`);
-                    return chrome.storage.local.set({[this.key] : newValue});
-                }
-            };
-
         async function createValueHandler(key, defaultValue)
         {
             let value = (await chrome.storage.local.get(key))[key];
